64b781169ad2d4ba1d0eb256cb7bdf025da8271b
aambo55 <aambo55@hotmail. om>
2019-10-21 08:47:09 +0700

    Update test.php

--- test.php
+++ test.php
@@ -2,9 +2,8 @@
    preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
  //  print_r($matches)."<br>";
    for($i = 0; $i < count($matches); $i++) {
-	   print $i;
         for($x = 0; $x < count($matches[$i]); $x++) {
-          print $matches[$i][$x];
+          print $matches[$i][$x]."<br>";
 
 		}
     }
