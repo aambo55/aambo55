bcbeb72fcc158a2e66774dd815e1bca3a9348af6
aambo55 <aambo55@hotmail. om>
2019-10-21 07:45:06 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,12 @@
 <?php
    preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
-   print_r($matches);
+   print_r($matches)."<br>";
+  print $matches[1][0]."<br>";
+  print $matches[2][0]."<br>";
+  print $matches[3][0]."<br>";
+ //  foreach($matches as $val){
+ //      $text = $val[0];
+//   }
 ?>
+
+Array ( [0] => Array ( [0] => foobarbaz [1] => 0 ) [1] => Array ( [0] => foo [1] => 0 ) [2] => Array ( [0] => bar [1] => 3 ) [3] => Array ( [0] => baz [1] => 6 ) ) 
