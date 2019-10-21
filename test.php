1f6dff9a05aeab89597c92baca4f5b8aed2731f3
aambo55 <aambo55@hotmail. om>
2019-10-21 08:45:53 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
-   preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_PATTERN_ORDER);
+   preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
  //  print_r($matches)."<br>";
-   for ($i = 0; $i < count($matches); $i++) {
-        for ($x = 0; $x < count($matches[$i]); $x++) {
+   for($i = 0; $i < count($matches); $i++) {
+	   print $i;
+        for($x = 0; $x < count($matches[$i]); $x++) {
           print $matches[$i][$x];
 
 		}
