32054840a15bbf19e7d21b4da431dfd46587d1d3
aambo55 <aambo55@hotmail. om>
2019-10-23 08:39:04 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,10 @@
 <?php
-require 'phpMQTT.php';
+require("../phpMQTT.php");
 $server = "m11.cloudmqtt.com";     // change if necessary
 $port = 16214;                     // change if necessary
 $username = "aambo55";                   // set your username
 $password = "rukyonaja13";                   // set your password
-$client_id = "ClientID".rand(); // make sure this is unique for connecting to sever - you could use uniqid()
+$client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 $mqtt = new phpMQTT($server, $port, $client_id);
 if ($mqtt->connect(true, NULL, $username, $password)) {
 	$mqtt->publish("/message", "LEDON", 0);
