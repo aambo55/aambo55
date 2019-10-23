380fc69a0858f5d5a536695fff41fdbef76904da
aambo55 <aambo55@hotmail. om>
2019-10-23 15:56:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,15 +50,6 @@ if ( sizeof($deCode['events']) > 0 ) {
 
 
 		//*************************************
-		$text_select = $text;
-preg_match('/(เปิดปั๊ม)/', $text_select, $pumpx, PREG_OFFSET_CAPTURE);
-//if($pumpx[0][0]=="เปิดปั๊ม"){
-?>
-	<script type="text/ja/ascript">
-        var xx = "LEDOFF";
-    </script>
-<?php
-//}
 
         //แปลงรหัสให้เพื่อให้โปรแกรมเอามาเปรียบเทียบได้
 		
@@ -77,7 +68,15 @@ preg_match('/(เปิดปั๊ม)/', $text_select, $pumpx, PREG_OFFSET_CAPTURE);
 
      }
 }
-
+		$text_select = $text;
+preg_match('/(เปิดปั๊ม)/', $text_select, $pumpx, PREG_OFFSET_CAPTURE);
+//if($pumpx[0][0]=="เปิดปั๊ม"){
+?>
+	<script type="text/ja/ascript">
+        var xx = "LEDOFF";
+    </script>
+<?php
+//}
 echo "<br> OK <br>";
 ?>
 <script>
