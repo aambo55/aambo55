2c0a1ee5930bbf82d5caa7433ab7db579537b387
aambo55 <aambo55@hotmail. om>
2019-10-24 10:28:46 +0700

    Update test3.php

--- test3.php
+++ test3.php
@@ -2,9 +2,9 @@
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "1";
-        $arrayPostData['messages'][1]['stickerId'] = "131";
+       // $arrayPostData['messages'][1]['type'] = "sticker";
+      //  $arrayPostData['messages'][1]['packageId'] = "1";
+      //  $arrayPostData['messages'][1]['stickerId'] = "131";
 
 print_r($arrayPostData);
 ?>
