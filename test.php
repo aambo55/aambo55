b393fe48b346d1a326b1b1bfe0860f0cd0c100e3
aambo55 <aambo55@hotmail. om>
2019-11-04 10:14:33 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <?php
   $text = "Bin2,setting, 1,5 ,33;ffgghh";
   preg_match("/((^[B|b][I|i][N|n])\d.*)(;)/", $text,$data);
+  if ($data[2} == '[B|b][I|i][N|n]'){
+    print "rrr".$dat[2];
+  }
   print_r($data);
 ?>
