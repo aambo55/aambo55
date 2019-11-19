7e42c16111bd9e207878201fe8fdc694cf2e6940
aambo55 <aambo55@hotmail. om>
2019-11-19 08:17:52 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-  $text = "Bin2,11,22,33;";
-  preg_match("/(^[B|b]in)(\d)(.*)(;)/", $text,$data);
+  $text = "Bin2 on";
+  preg_match("/(^[B|b]in)(\d)(.on)/", $text,$data);
 
   print_r($data);
 ?>
