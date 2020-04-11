840e7b96186a8e235ea1eac202fcf5a22f90b9f5
aambo55 <aambo55@hotmail. om>
2020-04-11 09:07:33 +0700

    Update eng.php

--- eng.php
+++ eng.php
@@ -1,8 +1,8 @@
 <?php
 
-   echo 'helo world Nichaphat Saefung\n';
-   echo "　　　\n";
-   echo 'good girl\n';
-   echo "dddddd\n";
+   echo 'helo world Nichaphat Saefung<br/>';
+   echo "　　　<br/>";
+   echo 'good girl<br/>';
+   echo "dddddd<br/>";
 
 ?>
